ff8eb7be9926b6ac9e43a15b3d0ea0a56f836456
IsadoraRochaB <isadorarochabrito.rb@gmail.com>
2022-05-31 10:44:52 -0300

    aula errado 2!

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+// auto load
+spl_autoload_extensions('.php');
+function classLoader($class)
+{
+  $nomeArquivo = $class . '.php';
+  $pastas = array(
+    "shared/controller",
+    "shared/model",
+    "public/controller",
+    "public/model"
+  );
+  foreach ($pastas as $pasta) {
+    $arquivo = "{$pasta}/{$nomeArquivo}";
+    if (file_exists($arquivo)) {
+      require_once($arquivo);
+    }
+  }
+}
+spl_autoload_register('classLoader');
+
+Session::startSession();
+Session::freeSession();
+
+// Front Controller
+class Aplicacao
+{
+  private static $app = "/Isacerto";
+  public static function run()
+  {
+    $layout = new Template('public/view/layout.html');
+    if (isset($_GET["class"])) {
+      $class = $_GET["class"];
+    } else {
+      $class = "Login";
+    }
+    if (isset($_GET["method"])) {
+      $method = $_GET["method"];
+    } else {
+      $method = "";
+    }
+    if (class_exists($class)) {
+      $pagina = new $class();
+      if (method_exists($pagina, $method)) {
+        $pagina->$method();
+      } else {
+        $pagina->controller();
+      }
+      $layout->set("uri", self::$app);
+      $layout->set('conteudo', $pagina->getMessage());
+    }
+    echo $layout->saida();
+  }
+}
+Aplicacao::run();
